6e8bd93422d98cfac41984ac2b2b8e2f8a474026
Justin Ireland <justin@justinireland.com>
2014-09-10 12:20:23 -0700

    Added videojs and client script boilerplate

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,47 @@
 'use strict';
 
+var debug = true;
 var socket = io();
+// VideoJS
+var	videojsConfig = {
+	"autoplay": false,
+	"controls": false,
+	"preload": "auto",
+	"techOrder": ["html5"]
+};
 
 // DOM ready
 $(function() {
+
+	var defaultBehaviors = (function(){
+
+		// Disable right click
+		$(document).bind('contextmenu', function(e) {
+			e.stopPropagation();
+			e.preventDefault();
+			e.stopImmediatePropagation();
+			return false;
+		});
+
+		// Disable dragging images
+		$('img').on('dragstart', function(event) { event.preventDefault(); });
+
+		// Enable cursor
+		if(debug){$('html').css('cursor','default');}
+
+	})();
+
+	// Initialization tasks
+	(function init(){
+
+		if(debug){
+			console.log('Debug mode ON');
+		} else console.log('Debug mode OFF');
+
+	})();
+
+	/*** Main ***/
+
 
 });
 
